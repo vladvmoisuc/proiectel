388cd69f24c03d1d92210fa4c9476ab38e3fed76
Moisuc Vasile Vlad <moisuc.vlad1@gmail.com222>
2021-06-13 04:42:47 +0300

    Remove Helmet to make the routing to work in prod.

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const helmet = require('helmet');
 const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
-app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './containers/Home';
 import Article from './containers/Article';
 import About from './containers/About';
@@ -13,26 +8,21 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/blog" />
-          </Route>
-          <Route path="/blog" exact>
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/blog/:article">
-            <Article />
-          </Route>
-          <Route path="/not-found">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/blog/:article">
+          <Article />
+        </Route>
+        <Route path="/not-found">
+          <NotFound />
+        </Route>
+      </Switch>
     </>
   );
 }
